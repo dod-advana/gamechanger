59ff1d4b70d220d7519748c9bc0f044370e10c8f
Eddie Storm <5409647+vctrstrm@users.noreply.github.com>
2021-11-08 23:16:14 -0800

    Adds simplified local ingest script

--- deploy/local-ingest.sh
+++ deploy/local-ingest.sh
@@ -9,7 +9,24 @@ DEPLOY_CONF="${SCRIPT_DIR}/deploy.conf.sh"
 source "$DEPLOY_CONF"
 
 function main() (
-  bash "${REPO_DIR_MAP[gamechanger-data]}/paasJobs/jobs/local_upload.sh" "$@"
+  local bucket_name=${S3_BUCKET_NAME:-advana-data-zone}
+  local es_index_name=${ES_INDEX_NAME:-gamechanger_original}
+  local es_alias_name=${ES_ALIAS_NAME:-gamechanger}
+
+  local raw_doc_dir=${1}
+  local parsed_doc_dir=${2:-}
+  local container_raw_doc_dir=/tmp/raw
+  local container_parsed_doc_dir=/tmp/parsed
+
+  compose_wrapper run \
+    ${S3_BUCKET_NAME:+-e S3_BUCKET_NAME=${S3_BUCKET_NAME}} \
+    ${ES_INDEX_NAME:+-e ES_INDEX_NAME=${ES_INDEX_NAME}} \
+    ${ES_ALIAS_NAME:+-e ES_ALIAS_NAME=${ES_ALIAS_NAME}} \
+    ${raw_doc_dir:+-v "${raw_doc_dir}:${container_raw_doc_dir}"} \
+    ${parsed_doc_dir:+-v "${parsed_doc_dir}:${container_parsed_doc_dir}"} \
+    -- _data_pipelines_cmd ./dataPipelines/scripts/local_ingest.sh \
+      ${raw_doc_dir:+-v "${container_raw_doc_dir}"} \
+      ${parsed_doc_dir:+-v "${container_parsed_doc_dir}"}
 )
 
 main "$@"
